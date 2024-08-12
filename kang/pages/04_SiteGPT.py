fe7540b51060032a39fa85e575c5c64666daabc5
SIGIRACE <chickang9@gmail.com>
2024-08-12 14:43:03 +0900

    10.3 Parsing Fucntion, by kang

--- kang/pages/04_SiteGPT.py
+++ kang/pages/04_SiteGPT.py
@@ -1,21 +1,35 @@
-from langchain.document_loaders import SitemapLoader
+from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_transformers import Html2TextTransformer
 import streamlit as st
-
-
-@st.cache_data(show_spinner="Loading website...")
-def load_website(url):
-    loader = SitemapLoader(url,
-                           filter_urls=[r"^(.*\/blog\/).*"])
-    loader.requests_per_second = 5
-    docs = loader.load()
-    return docs
-
+from bs4 import BeautifulSoup
 
 st.set_page_config(
     page_title="SiteGPT",
     page_icon="🖥️",
 )
 
+base_url = "https://www.fmkorea.com"
+
+html2text_transformer = Html2TextTransformer()
+
+@st.cache_data(show_spinner="Loading website...")
+def load_website(id_list):
+    docs = []
+    for id in id_list:
+        loader = AsyncChromiumLoader([base_url + "/" + id.strip()])
+        doc = loader.load()
+        # HTML 파싱
+        soup = BeautifulSoup(doc[0].page_content, 'html.parser')
+        
+        # header와 footer 제거
+        if soup.header:
+            soup.header.decompose()
+        if soup.footer:
+            soup.footer.decompose()
+        
+        str(soup.get_text())
+
+    return docs
 
 st.markdown(
     """
@@ -27,18 +41,13 @@ st.markdown(
 """
 )
 
-
 with st.sidebar:
-    url = st.text_input(
+    id_list = st.text_input(
         "Write down a URL",
-        placeholder="https://example.com",
+        placeholder="Doc id",
     )
 
-
-if url:
-    if ".xml" not in url:
-        with st.sidebar:
-            st.error("Please write down a Sitemap URL.")
-    else:
-        docs = load_website(url)
-        st.write(docs)
+if id_list:
+    id_list = id_list.split(",")
+    docs = load_website(id_list)
+    st.write(docs)
